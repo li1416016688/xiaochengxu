b5780d695044ec2dc694711695cafb0432af4d17
lihanxin <1443838220@qq.com>
2020-07-24 17:15:42 +0800

    分享

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,15 @@
 // pages/home/home.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
+  onShareAppMessage: function(options) {
+    return {
+      title: '你好',
+      path: 'pages/about/about.wxml',
+      imageUrl: 'https://pics4.baidu.com/feed/574e9258d109b3de8781d8114cc26e86800a4c14.jpeg?token=8e39aec6ea681f8bf69438ff8b3e9155'
+    }
   }
 })
